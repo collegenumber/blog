ec466aa2bd3a2372a4c8101f30be25f11191cb87
collegenumber <yamayama.283ky@gmail.com>
2021-11-06 10:27:05 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
+
